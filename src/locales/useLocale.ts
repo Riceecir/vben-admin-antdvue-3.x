92d41230ce183cf40a0824d6da9b9ba3dbdad72a
林枫帆 <lin.fengfan@i-jimi.com.cn>
2022-04-21 16:45:30 +0800

    moment => dayjs

--- src/locales/useLocale.ts
+++ src/locales/useLocale.ts
@@ -3,7 +3,7 @@
  */
 import type { LocaleType } from '/#/config';
 
-import moment from 'moment';
+import dayjs from 'dayjs';
 
 import { i18n } from './setupI18n';
 import { useLocaleStoreWithOut } from '/@/store/modules/locale';
@@ -56,7 +56,7 @@ export function useLocale() {
     const { message, momentLocale, momentLocaleName } = langModule;
 
     globalI18n.setLocaleMessage(locale, message);
-    moment.updateLocale(momentLocaleName, momentLocale);
+    dayjs.locale(momentLocaleName, momentLocale);
     loadLocalePool.push(locale);
 
     setI18nLanguage(locale);
